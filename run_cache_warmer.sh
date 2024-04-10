ec8530511b657c8e9361aed24e5f29aea8cc853c
Joz Ahmed <jozzy_a@hotmail.com>
2024-04-10 22:13:28 +0100

    Add a shell wraper for docker

---
+++ run_cache_warmer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+BASE_URL=$1
+
+
+if [[ -z "$BASE_URL" ]]; then
+  echo "Error: Please provide a BASE_URL as an argument in the format https://your_site.com/"
+  exit 1
+fi
+
+docker run --name web-cache-warmer -e BASE_URL="$BASE_URL" -v $(pwd)/csv/urls.csv:/app/urls.csv cache-warmer
+docker rm web-cache-warmer
+
